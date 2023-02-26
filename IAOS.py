f5c4a69e09b8eb67c147f1b05a1c54d0cd7d4002
rubenixter <124615627+rubenixter@users.noreply.github.com>
2023-02-26 12:15:50 +0100

    Add files via upload
    First functional version of the program

---
+++ IAOS.py
@@ -0,0 +1,77 @@
+"""
+Author: Rubén Llorente Canet 
+"""
+
+import os
+import requests
+import re
+from grobid_client.grobid_client import GrobidClient
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+# Directory where the PDF files are stored
+pdf_dir = "./ToAnalize"
+fig_dir="./Figures"
+nums=[]
+
+i=1
+# Grobid API endpoint
+grobid_api = "http://localhost:8070/api/processFulltextDocument"
+client = GrobidClient(config_path="./config.json")
+headers = {'Accept': 'application/xml'}
+# Loop over all PDF files in the directory
+for file_name in os.listdir(pdf_dir):
+    urls = []
+    if file_name.endswith(".pdf"):
+        # Get the binary data of the PDF file
+        file_path = os.path.join(pdf_dir, file_name)
+        with open(file_path, "rb") as f:
+            pdf_data = f.read()
+        respuesta =client.post(grobid_api, headers=headers, files={'input': ('input.pdf', pdf_data)})
+        fres=respuesta[0].text.encode('utf-8')
+        results = re.findall(r"<abstract>(.*?)</abstract>", str(fres), re.DOTALL)
+    
+        if results:
+            for result in results:
+                abs = re.findall(r"<p>(.*?)</p>", str(result), re.DOTALL)
+                abstract  = abs[0].encode("utf-8").decode("utf-8")
+                wordcloud = WordCloud(width=800, height=800, background_color='white').generate(abstract)
+
+                # plot the word cloud
+                #plt.figure(figsize=(6,6), facecolor=None)
+                plt.title('Cloud'+str(i))
+                plt.imshow(wordcloud)
+                plt.axis("off")
+                plt.tight_layout(pad=0)
+                
+                #plt.show()
+                plt.savefig(fig_dir+'/Wordcloud'+str(i)+'.png')
+                
+    else:
+        print("No results found")
+
+    figure_count = len(re.findall(r"</figure>", str(fres), re.DOTALL))
+    nums.append(figure_count)
+
+    words = re.findall(r'target=(\w+)',  str(fres), re.DOTALL)
+    if words:
+        # If matches are found, add them to the url_list
+        urls.extend(words)
+
+    if urls:
+        # Write URLs to a text file in the "results" folder
+        with open('./Figures/urls'+str(i)+'.txt', 'w') as file:
+            for url in urls:
+                file.write(url + '\n')
+    i=i+1
+            
+nombres=['Fichero 1','Fichero 2']
+plt.figure(figsize=(6,6), facecolor=None)
+plt.title('bars')
+plt.bar(nombres,nums)
+
+
+plt.xlabel("Number")
+print(nums)
+#plt.show()
+plt.savefig(fig_dir+'/Bars'+'.png')
